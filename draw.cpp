1dc1a3ba417adcf3abe0dc69b9f67292682eaea6
Axel Lells Hjelmqvist <axelhjq@gmail.com>
2021-02-13 17:52:59 +0100

    Turn texture coordinates upside down

--- draw.cpp
+++ draw.cpp
@@ -161,12 +161,12 @@ static GLfloat squareVertices[] = {
 };
 
 static GLfloat squareTexCoords[] = {
-    0.0f, 0.0f,
-    1.0f, 1.0f,
     0.0f, 1.0f,
-    0.0f, 0.0f,
     1.0f, 0.0f,
-    1.0f, 1.0f
+    0.0f, 0.0f,
+    0.0f, 1.0f,
+    1.0f, 1.0f,
+    1.0f, 0.0f
 };
 
 static GLfloat squareNormals[] = {
